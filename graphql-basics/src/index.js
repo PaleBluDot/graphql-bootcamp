d44e1a41fe2efff3fc42cae6d638e6c06294cd4a
Pavel Sanchez <pavel@bluedot.dev>
2020-12-12 15:51:48 -0500

    :sparkles: Deleting Data w/ Mutation 2

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -80,8 +80,10 @@ const typeDefs = `
 		createUser(data: CreateUserInput!): User!
 		deleteUser(id: ID!): User!
 		createPost(data: CreatePostInput!): Post!
+		deletePost(id: ID!): Post!
 		createComment(data: CreateCommentInput!): Comment!
-	}
+		deleteComment(id: ID!): Comment!
+}
 
 	input CreateUserInput {
 		name: String!
@@ -222,6 +224,19 @@ const resolvers = {
 
 			return post
 		},
+		deletePost(parent, args, ctx, info) {
+			const postIndex = posts.findIndex((post) => post.id === args.id)
+
+			if(postIndex === -1) {
+				throw new Error('Post not found')
+			}
+
+			const deletedPosts = posts.splice(postIndex, 1)
+
+			comments = comments.filter((comment) => comment.post !== args.id)
+
+			return deletedPosts[0]
+		},
 		createComment(parent, args, ctx, info) {
 			const userExists = users.some((user) => user.id === args.data.author)
 			const postExists = posts.some((post) => post.id === args.data.post && post.published)
@@ -236,9 +251,22 @@ const resolvers = {
 			}
 
 			comments.push(comment)
+
 			return comment
+		},
+		deleteComment(parent, args, ctx, info) {
+			const commentIndex = comments.findIndex((comment) => comment.id === args.id)
+
+			if (commentIndex === -1) {
+				throw new Error('Comment not found')
+			}
+
+			const deletedComments = comments.splice(commentIndex, 1)
+
+			return deletedComments[0]
 		}
 	},
+
 	Post: {
 		author(parent, args, ctx, info) {
 			return users.find((user) => {
